fb36a04cd362405442660cee6b0bdcb6035bb618
Ira Akulova <ira.akulova2019@gmail.com>
2023-06-26 11:21:33 +0300

    added btns in header

--- src/components/sharedLayout/SharedLayout.jsx
+++ src/components/sharedLayout/SharedLayout.jsx
@@ -2,24 +2,35 @@ import { Outlet, NavLink } from 'react-router-dom';
 import { Suspense } from 'react';
 import css from './SharedLayout.module.css';
 import Loader from '../../components/loader/Loader';
+import Button from '../button/Button';
 
 const SharedLayout = () => {
   return (
     <div>
-      <nav className={css.navigation}>
-        <NavLink to="/" className={css.link}>
-          Home
-        </NavLink>
-        {/* <NavLink to="/movies" className={css.link}>
+      <div className={css.sharedHeader}>
+        <nav className={css.navigation}>
+          <NavLink to="/" className={css.link}>
+            Home
+          </NavLink>
+          {/* <NavLink to="/movies" className={css.link}>
           Movies
         </NavLink> */}
-        <NavLink to="/portfolio" className={css.link}>
-          Portfolio
-        </NavLink>
-        <NavLink to="/contacts" className={css.link}>
-          Contacts
-        </NavLink>
-      </nav>
+          <NavLink to="/portfolio" className={css.link}>
+            Portfolio
+          </NavLink>
+          <NavLink to="/contacts" className={css.link}>
+            Contacts
+          </NavLink>
+        </nav>
+        <ul className={css.authBtns}>
+          <li>
+            <Button text={"Register"} />
+          </li>
+          <li>
+            <Button text={"Login"} />
+          </li>
+        </ul>
+      </div>
       <Suspense fallback={<Loader />}>
         <Outlet />
       </Suspense>
