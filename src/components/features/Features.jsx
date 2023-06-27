e75bdb6a29bad94537875fd063669f1729a62cf4
Ira Akulova <ira.akulova2019@gmail.com>
2023-06-27 11:49:04 +0300

    added features deco

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,33 +1,33 @@
-// import css from "./Features.module.css";
+import css from "./Features.module.css";
 
 const Features = () => {
     return (
-        <div>
-            <h2>Why Choosing Us</h2>
-            <ul>
-                <li>
-                    <h4>Luxury facilities</h4>
-                    <p>
-                        The advantage of hiring a workspace with us is that givees you
-                        comfortable service and all-around facilities.
-                    </p>
-                </li>
-                <li>
-                    <h4>Affordable Price</h4>
-                    <p>
-                        You can get a workspace of the highst quality at an affordable
-                        price and still enjoy the facilities that are oly here.
-                    </p>
-                </li>
-                <li>
-                    <h4>Many Choices</h4>
-                    <p>
-                        We provide many unique work space choices so that you can choose
-                        the workspace to your liking.
-                    </p>
-                </li>
-            </ul>
-        </div>
+      <div className={css.featuresContainer}>
+        <h2 className={css.featuresHeader}>Why Choosing Us</h2>
+        <ul className={css.features}>
+          <li className={css.feature}>
+            <h4 className={css.featureTitle}>Luxury facilities</h4>
+            <p className={css.featureDesc}>
+              The advantage of hiring a workspace with us is that givees you
+              comfortable service and all-around facilities.
+            </p>
+          </li>
+          <li className={css.feature}>
+            <h4 className={css.featureTitle}>Affordable Price</h4>
+            <p className={css.featureDesc}>
+              You can get a workspace of the highst quality at an affordable
+              price and still enjoy the facilities that are oly here.
+            </p>
+          </li>
+          <li className={css.feature}>
+            <h4 className={css.featureTitle}>Many Choices</h4>
+            <p className={css.featureDesc}>
+              We provide many unique work space choices so that you can choose
+              the workspace to your liking.
+            </p>
+          </li>
+        </ul>
+      </div>
     );
 };
 
