f9b3c699403a03354a298f84890be8eebf15ad74
Ira Akulova <ira.akulova2019@gmail.com>
2023-06-28 09:19:04 +0300

    devided portfolio to components

---
+++ src/components/ourWorks/OurWorks.jsx
@@ -0,0 +1,31 @@
+const OurWorks = () => {
+  return (
+    <section>
+      <h2>Our works</h2>
+      <ul>
+        <li>
+          <img src="#" alt="" />
+          <h5>Name</h5>
+          <p>Description</p>
+        </li>
+        <li>
+          <img src="#" alt="" />
+          <h5>Name</h5>
+          <p>Description</p>
+        </li>
+        <li>
+          <img src="#" alt="" />
+          <h5>Name</h5>
+          <p>Description</p>
+        </li>
+        <li>
+          <img src="#" alt="" />
+          <h5>Name</h5>
+          <p>Description</p>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default OurWorks;
---
+++ src/components/reviwes/Reviwes.jsx
@@ -0,0 +1,55 @@
+const Reviwes = () => {
+  return (
+    <section>
+      <h2>Our Client Reviews</h2>
+      <ul>
+        <li>
+          <div>
+            <div>
+              <img src="#" alt="" />
+              <h6>Bang Upin</h6>
+              <p>
+                Bang Upin Pedagang Asongan “Terimakasih banyak, kini ruanganku
+                menjadi lebih mewah dan terlihat mahal“
+              </p>
+              <div>
+                <span>Stars</span>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div>
+            <div>
+              <img src="#" alt="" />
+              <h6>Ibuk Sukijan</h6>
+              <p>
+                “Makasih Panto, aku sekarang berasa tinggal di apartment karena
+                barang-barang yang terlihat mewah“
+              </p>
+              <div>
+                <span>Stars</span>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div>
+            <div>
+              <img src="#" alt="" />
+              <h6>Mpok Ina</h6>
+              <p>
+                “Sangat terjangkau untuk kantong saya yang tidak terlalu banyak“
+              </p>
+              <div>
+                <span>Stars</span>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default Reviwes;
