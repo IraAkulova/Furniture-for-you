c4043e255fab0a8478adad7df8d64646db7ecb42
Ira Akulova <ira.akulova2019@gmail.com>
2023-06-30 11:30:15 +0300

    added mobile styles to reviews

--- src/components/reviewes/Reviewes.jsx
+++ src/components/reviewes/Reviewes.jsx
@@ -6,7 +6,12 @@ const Reviewes = () => {
       <h2 className={css.reviwesHeader}>Our Client Reviews</h2>
       <ul className={css.reviwes}>
         <li className={css.reviweCard}>
-          <img src="./photo1.jpg" alt="Smiling man" width="250" />
+          <img
+            src="./photo1.jpg"
+            alt="Smiling man"
+            width="250"
+            className={css.reviweImg}
+          />
           <h6 className={css.name}>Bang Upin</h6>
           <p className={css.reviwe}>
             Bang Upin Pedagang Asongan “Terimakasih banyak, kini ruanganku
@@ -15,7 +20,12 @@ const Reviewes = () => {
           <div className={css.stars}>Stars</div>
         </li>
         <li className={css.reviweCard}>
-          <img src="./photo2.jpg" alt="Girl" width="250" />
+          <img
+            src="./photo2.jpg"
+            alt="Girl"
+            width="250"
+            className={css.reviweImg}
+          />
           <h6 className={css.name}>Bang Upin</h6>
           <p className={css.reviwe}>
             Bang Upin Pedagang Asongan “Terimakasih banyak, kini ruanganku
@@ -24,7 +34,12 @@ const Reviewes = () => {
           <div className={css.stars}>Stars</div>
         </li>
         <li className={css.reviweCard}>
-          <img src="./photo3.jpg" alt="Man" width="250" />
+          <img
+            src="./photo3.jpg"
+            alt="Man"
+            width="250"
+            className={css.reviweImg}
+          />
           <h6 className={css.name}>Ibuk Sukijan</h6>
           <p className={css.reviwe}>
             “Makasih Panto, aku sekarang berasa tinggal di apartment karena
@@ -33,7 +48,12 @@ const Reviewes = () => {
           <div className={css.stars}>Stars</div>
         </li>
         <li className={css.reviweCard}>
-          <img src="./photo4.jpg" alt="Man in glasses" width="250" />
+          <img
+            src="./photo4.jpg"
+            alt="Man in glasses"
+            width="250"
+            className={css.reviweImg}
+          />
           <h6 className={css.name}>Mpok Ina</h6>
           <p className={css.reviwe}>
             “Sangat terjangkau untuk kantong saya yang tidak terlalu banyak“
