25a16dcf979d16cab2e1860797a2193c44c24a7b
Ira Akulova <ira.akulova2019@gmail.com>
2023-06-30 11:57:10 +0300

    added mobile styles to contacts

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -10,29 +10,32 @@ const Contacts = () => {
         height="450"
         loading="lazy"
         referrerpolicy="no-referrer-when-downgrade"
+        className={css.contactsMap}
       ></iframe>
-      <ul className={css.contactsTitles}>
-        <li>Наша адреса:</li>
-        <li>Email:</li>
-        <li>Телефон:</li>
-      </ul>
-      <ul className={css.contacts}>
-        <li className={css.contact}>
-          <a
-            href="https://www.google.com/maps/place/%D0%9A%D0%B0%D0%BC'%D1%8F%D0%BD%D0%BA%D0%B0-%D0%91%D1%83%D0%B7%D1%8C%D0%BA%D0%B0,+%D0%9B%D1%8C%D0%B2%D1%96%D0%B2%D1%81%D1%8C%D0%BA%D0%B0+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+80400/@50.1050243,24.3116199,13z/data=!3m1!4b1!4m6!3m5!1s0x472532b5ea1f35af:0x4872baad1d0cd1b2!8m2!3d50.0994726!4d24.3454482!16zL20vMGNnajRu?entry=ttu"
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-          >
-            м. Кам'янка-Бузька
-          </a>
-        </li>
-        <li className={css.contact}>
-          <a href="mailto:info@devstudio.com">info@furniture4u.com</a>
-        </li>
-        <li className={css.contact}>
-          <a href="tel:+380961111111">+38 096 111 11 11</a>
-        </li>
-      </ul>
+      <div className={css.contacts}>
+        <ul className={css.contactsTitles}>
+          <li>Наша адреса:</li>
+          <li>Email:</li>
+          <li>Телефон:</li>
+        </ul>
+        <ul className={css.contactsList}>
+          <li className={css.contact}>
+            <a
+              href="https://www.google.com/maps/place/%D0%9A%D0%B0%D0%BC'%D1%8F%D0%BD%D0%BA%D0%B0-%D0%91%D1%83%D0%B7%D1%8C%D0%BA%D0%B0,+%D0%9B%D1%8C%D0%B2%D1%96%D0%B2%D1%81%D1%8C%D0%BA%D0%B0+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+80400/@50.1050243,24.3116199,13z/data=!3m1!4b1!4m6!3m5!1s0x472532b5ea1f35af:0x4872baad1d0cd1b2!8m2!3d50.0994726!4d24.3454482!16zL20vMGNnajRu?entry=ttu"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+            >
+              м. Кам'янка-Бузька
+            </a>
+          </li>
+          <li className={css.contact}>
+            <a href="mailto:info@devstudio.com">info@furniture4u.com</a>
+          </li>
+          <li className={css.contact}>
+            <a href="tel:+380961111111">+38 096 111 11 11</a>
+          </li>
+        </ul>
+      </div>
     </address>
   );
 };
