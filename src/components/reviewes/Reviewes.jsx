63461393a6f9426afef73086d41a465f713a25d3
Ira Akulova <ira.akulova2019@gmail.com>
2023-07-06 11:24:24 +0300

    added reviews fetch

--- src/components/reviewes/Reviewes.jsx
+++ src/components/reviewes/Reviewes.jsx
@@ -1,61 +1,69 @@
 import css from "./Reviewes.module.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+axios.defaults.baseURL = "https://furniture4u.onrender.com";
 
 const Reviewes = () => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    axios
+      .get("/api/reviews")
+      .then((response) => {
+        setReviews((prevRev) => [...prevRev, ...response.data.data.reviews]);
+      })
+      .catch((error) => console.log(error.message));
+  }, []);
+
+console.log(reviews);
+
   return (
-    <section className={css.reviwesContainer}>
-      <h2 className={css.reviwesHeader}>Our Client Reviews</h2>
-      <ul className={css.reviwes}>
-        <li className={css.reviweCard}>
-          <img
-            src="./photo1.jpg"
-            alt="Smiling man"
-            width="250"
-            className={css.reviweImg}
-          />
-          <h6 className={css.name}>Bang Upin</h6>
-          <p className={css.reviwe}>
-            Bang Upin Pedagang Asongan “Terimakasih banyak, kini ruanganku
-            menjadi lebih mewah dan terlihat mahal“
-          </p>
-          <div className={css.stars}>Stars</div>
-        </li>
-        <li className={css.reviweCard}>
+    <section className={css.reviewsContainer}>
+      <h2 className={css.reviewsHeader}>Our Client Reviews</h2>
+      <ul className={css.reviews}>
+        {reviews.map(({ _id, name, comment }) => (
+          <li className={css.reviewCard} key={_id}>
+            <h6 className={css.name}>{name}</h6>
+            <p className={css.review}>{comment}</p>
+          </li>
+        ))}
+        <li className={css.reviewCard}>
           <img
             src="./photo2.jpg"
             alt="Girl"
             width="250"
-            className={css.reviweImg}
+            className={css.reviewImg}
           />
           <h6 className={css.name}>Bang Upin</h6>
-          <p className={css.reviwe}>
+          <p className={css.review}>
             Bang Upin Pedagang Asongan “Terimakasih banyak, kini ruanganku
             menjadi lebih mewah dan terlihat mahal“
           </p>
           <div className={css.stars}>Stars</div>
         </li>
-        <li className={css.reviweCard}>
+        <li className={css.reviewCard}>
           <img
             src="./photo3.jpg"
             alt="Man"
             width="250"
-            className={css.reviweImg}
+            className={css.reviewImg}
           />
           <h6 className={css.name}>Ibuk Sukijan</h6>
-          <p className={css.reviwe}>
+          <p className={css.review}>
             “Makasih Panto, aku sekarang berasa tinggal di apartment karena
             barang-barang yang terlihat mewah“
           </p>
           <div className={css.stars}>Stars</div>
         </li>
-        <li className={css.reviweCard}>
+        <li className={css.reviewCard}>
           <img
             src="./photo4.jpg"
             alt="Man in glasses"
             width="250"
-            className={css.reviweImg}
+            className={css.reviewImg}
           />
           <h6 className={css.name}>Mpok Ina</h6>
-          <p className={css.reviwe}>
+          <p className={css.review}>
             “Sangat terjangkau untuk kantong saya yang tidak terlalu banyak“
           </p>
           <div className={css.stars}>Stars</div>
