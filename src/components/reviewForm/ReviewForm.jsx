f01b93d2f71d35352ae69584c15bcf65b8047373
Ira Akulova <ira.akulova2019@gmail.com>
2023-07-07 14:35:49 +0300

    added modal for leaving comment

---
+++ src/components/reviewForm/ReviewForm.jsx
@@ -0,0 +1,121 @@
+import css from "../reviewForm/ReviewForm.module.css";
+import { useState } from "react";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://furniture4u.onrender.com";
+
+const ReviewForm = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [comment, setComment] = useState("");
+    const handleInputChange = (e) => {
+    switch (e.target.name) {
+      case "name":
+        setName(e.target.value);
+        break;
+      case "email":
+        setEmail(e.target.value);
+        break;
+      case "phone":
+        setPhone(e.target.value);
+        break;
+      case "comment":
+        setComment(e.target.value);
+        break;
+
+      default:
+        return;
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("/api/reviews", {
+        name,
+        comment,
+        email,
+        phone,
+      });
+      return response.data;
+    } catch (error) {
+      console.log(error.message);
+    }
+    setName("");
+    setComment("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={css.reviewForm}>
+      <label className={css.reviewLabel}>
+        Name
+        <br></br>
+        <input
+          type="text"
+          name="name"
+          value={name}
+        //   pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={handleInputChange}
+          className={css.reviewField}
+          placeholder="Type your name"
+        />
+      </label>
+      <label className={css.reviewLabel}>
+        Email
+        <br></br>
+        <input
+          type="email"
+          name="email"
+          value={email}
+        //   pattern="/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/"
+          title="Enter valid email"
+          required
+          onChange={handleInputChange}
+          className={css.reviewField}
+          placeholder="Type your name"
+        />
+      </label>
+      <label className={css.reviewLabel}>
+        Phone
+        <br></br>
+        <input
+          type="tel"
+          name="phone"
+          value={phone}
+        //   pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          onChange={handleInputChange}
+          className={css.reviewField}
+          placeholder="Type your name"
+        />
+      </label>
+      <label className={css.reviewLabel}>
+        Comment
+        <br></br>
+        <textarea
+          type="text"
+          name="comment"
+          value={comment}
+          maxLength="100"
+          required
+          onChange={handleInputChange}
+          className={css.reviewField}
+          placeholder="Type comment"
+          style={{
+            height: 80,
+            fontSize: 16,
+          }}
+        />
+      </label>
+      <button type="submit" className={css.reviewSubmBtn}>
+        Leave a comment
+      </button>
+    </form>
+  );
+};
+
+export default ReviewForm;
--- src/components/reviewes/Reviewes.jsx
+++ src/components/reviewes/Reviewes.jsx
@@ -1,6 +1,7 @@
 import css from "./Reviewes.module.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import ReviewModal from "../reviewModal/ReviewModal";
 
 axios.defaults.baseURL = "https://furniture4u.onrender.com";
 
@@ -15,10 +16,15 @@ const Reviewes = () => {
       .catch((error) => console.log(error.message));
   }, []);
 
-console.log(reviews);
+    const [showModal, setShowModal] = useState(false);
+
+    const toggleModal = () => {
+      setShowModal(!showModal);
+    };
 
   return (
     <section className={css.reviewsContainer}>
+      {showModal && <ReviewModal toggleModal={toggleModal} />}
       <h2 className={css.reviewsHeader}>Our Client Reviews</h2>
       <ul className={css.reviews}>
         {reviews.map(({ _id, name, comment, avatarURL }) => (
@@ -26,55 +32,23 @@ console.log(reviews);
             <img
               src={avatarURL}
               alt="Genereted avatar"
-              width="250"
+              width="200"
               className={css.reviewImg}
             />
             <h6 className={css.name}>{name}</h6>
             <p className={css.review}>{comment}</p>
           </li>
         ))}
-        <li className={css.reviewCard}>
-          <img
-            src="./photo2.jpg"
-            alt="Girl"
-            width="250"
-            className={css.reviewImg}
-          />
-          <h6 className={css.name}>Bang Upin</h6>
-          <p className={css.review}>
-            Bang Upin Pedagang Asongan “Terimakasih banyak, kini ruanganku
-            menjadi lebih mewah dan terlihat mahal“
-          </p>
-          <div className={css.stars}>Stars</div>
-        </li>
-        <li className={css.reviewCard}>
-          <img
-            src="./photo3.jpg"
-            alt="Man"
-            width="250"
-            className={css.reviewImg}
-          />
-          <h6 className={css.name}>Ibuk Sukijan</h6>
-          <p className={css.review}>
-            “Makasih Panto, aku sekarang berasa tinggal di apartment karena
-            barang-barang yang terlihat mewah“
-          </p>
-          <div className={css.stars}>Stars</div>
-        </li>
-        <li className={css.reviewCard}>
-          <img
-            src="./photo4.jpg"
-            alt="Man in glasses"
-            width="250"
-            className={css.reviewImg}
-          />
-          <h6 className={css.name}>Mpok Ina</h6>
-          <p className={css.review}>
-            “Sangat terjangkau untuk kantong saya yang tidak terlalu banyak“
-          </p>
-          <div className={css.stars}>Stars</div>
-        </li>
       </ul>
+      <button
+        type="button"
+        className={css.reviewBtn}
+        onClick={() => {
+          setShowModal(true);
+        }}
+      >
+        Add Review
+      </button>
     </section>
   );
 };
